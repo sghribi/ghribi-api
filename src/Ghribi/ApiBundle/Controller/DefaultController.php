421cb906c87c9d0aea54bcbe30c1cbcb7c1fd031
Samy Ghribi <samy.ghribi@student.ecp.fr>
2015-03-14 15:19:40 +0000

    Add whoami route

--- src/Ghribi/ApiBundle/Controller/DefaultController.php
+++ src/Ghribi/ApiBundle/Controller/DefaultController.php
@@ -2,9 +2,10 @@
 
 namespace Ghribi\ApiBundle\Controller;
 
+use Ghribi\ApiBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 /**
  * Class DefaultController
@@ -12,10 +13,18 @@ use Symfony\Component\HttpFoundation\Response;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/me")
      */
-    public function indexAction()
+    public function whoAmIAction()
     {
-        return new Response('OK');
+        /** @var User $user */
+        $user = $this->getUser();
+
+        /** @TODO: Use serializer */
+        return new JsonResponse(array(
+            'id'        => $user->getId(),
+            'email'     => $user->getEmail(),
+            'username'  => $user->getUsername(),
+        ));
     }
 }
