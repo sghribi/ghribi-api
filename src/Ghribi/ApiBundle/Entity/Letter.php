f94c085efebd4892092f9f18b30271cde2d49e38
Samy Ghribi <samy.ghribi@student.ecp.fr>
2015-03-15 17:53:08 +0000

    Add name

--- src/Ghribi/ApiBundle/Entity/Letter.php
+++ src/Ghribi/ApiBundle/Entity/Letter.php
@@ -33,6 +33,14 @@ class Letter
     private $text;
 
     /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     * @Serializer\Expose
+     */
+    private $name;
+
+    /**
      * Get id
      *
      * @return integer 
@@ -64,5 +72,29 @@ class Letter
     public function getText()
     {
         return $this->text;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Letter
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
     }
 }
